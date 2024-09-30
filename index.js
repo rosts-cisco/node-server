7021651dd900d5e2e2d2ef1abc83fc8e32601a85
rosts-cisco <98326687+rosts-cisco@users.noreply.github.com>
2024-09-30 21:04:58 +0200

    upd

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.header(
+    "Content-Security-Policy",
+    "frame-ancestors 'self' *.ngrok.io *.slido-staging.com:* studio.staging.socio.events studio.development.socio.events integration.vidcast.io localhost:*"
+  );
+  res.send(TEMPLATE);
+});
+
+let TEMPLATE = "";
+fs.readFile(path.resolve(__dirname, "template.html"), "utf8", (err, data) => {
+  TEMPLATE = data;
+  app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
+});
